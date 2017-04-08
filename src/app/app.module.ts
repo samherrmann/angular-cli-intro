3b3de9f6b4e4eb8f2fc43e468a08713620f8dc95
Sam Herrmann <samherrmann.github@gmail.com>
2017-04-08 15:14:37 -0400

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -9,23 +10,32 @@ import { EmphasizeDirective } from './emphasize.directive';
 import { TruncatePipe } from './truncate.pipe';
 import { LoggerService } from "./logger.service";
 import { GithubService } from "./github.service";
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     EmphasizeDirective,
-    TruncatePipe
+    TruncatePipe,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     LoggerService,
     GithubService
-
   ],
   bootstrap: [AppComponent]
 })
